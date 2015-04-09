0f07bcc3a6a268e48c1fa5ea70426856241e9171
Kristian Rekstad <krirek@msn.com>
2015-04-09 13:17:23 +0200

    folder restructureing. Added some code to xbot.ino

---
+++ src/xbot/XBot.ino
@@ -0,0 +1,50 @@
+
+#include "PLab_ZumoMotors.h"
+#include "Pushbutton.h"
+#include "QTRSensors.h"
+#include "ZumoBuzzer.h"
+#include "ZumoMotors.h"
+#include "ZumoReflectanceSensorArray.h"
+
+#include "PLabBTCallback.h"
+#include "PLabBTSerial.h"
+
+// STATES :^)
+#define ST_NORMAL 		1
+#define ST_SEARCH_LINE  2
+#define ST_SEARCH_AREA	3
+
+// Current state
+int state = ST_NORMAL;
+
+
+void setup() {
+    // put your setup code here, to run once:
+
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    delay(100);
+
+}
+
+void kos(){
+
+}
+
+void sint(){
+
+}
+
+void behov_sov(){
+
+}
+
+void behov_spis(){
+
+}
+
+void behov_vask(){
+	
+}
